230d9c9a0e84befa4fb6cb1947329c36790e22f3
Tizazab <tizazabayana2@gmail.com>
2025-05-23 11:04:35 -0700

    Initial commit

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Saved = () => {
+  return (
+    <View className="flex-1 bg-black items-center justify-center">
+      <Text className="text-white text-xl">Saved Screen</Text>
+    </View>
+  )
+}
+
+export default Saved
